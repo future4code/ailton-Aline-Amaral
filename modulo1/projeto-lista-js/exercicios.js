aeb40b8590f3b0c87dcfafeb558430a9bb40a4a0
Aline do Amaral <alinedoamaral13@gmail.com>
2022-05-15 09:10:08 -0300

    ultima versao

--- modulo1/projeto-lista-js/exercicios.js
+++ modulo1/projeto-lista-js/exercicios.js
@@ -1,4 +1,4 @@
-/* // EXEMPLOS DE IMPLEMENTAÇÃO ---------------------------------------------------------------
+// EXEMPLOS DE IMPLEMENTAÇÃO ---------------------------------------------------------------
 
 // EXERCÍCIO 0A
 function soma(num1, num2) {
@@ -13,64 +13,53 @@ function imprimeMensagem() {
 
   console.log(mensagem)
 }
- */
+
 // EXERCÍCIOS PARA FAZER ------------------------------------------------------------------
 
 // EXERCÍCIO 01
 function calculaAreaRetangulo(altura, largura) {
-  area = (altura * largura)/2
-  return area
+  const mensagem = parseFloat(prompt(`Digite um valor de altura`))
+  const mensagem2 = parseFloat(prompt(`Digite um valor de largura`))
+  const soma = mensagem * mensagem2
+  console.log(soma)
 }
-let valorAltura = parseFloat(prompt(`Digite a altura de um triângulo`))
-let valorLargura = parseFloat(prompt(`Digite a largura de um triângulo`))
 
-console.log(calculaAreaRetangulo(valorAltura, valorLargura))
 
 
 // EXERCÍCIO 02
 function imprimeIdade(anoAtual, anoNascimento) {
-  idade = anoAtual - anoNascimento
-  return idade
+  const mensagem = parseFloat(prompt(`Digite o ano em que estamos`))
+  const mensagem2 = parseFloat(prompt(`Digite de seu nascimento`))
+  const subtracao = mensagem - mensagem2
+  console.log(subtracao)
 }
-let hoje = parseFloat(prompt(`Digite em que ano estamos`))
-let nascimento = parseFloat(prompt(`Digite o ano de seu nascimento`))
-
-console.log(imprimeIdade(hoje, nascimento))
 
 
 // EXERCÍCIO 03
 function calculaIMC(peso, altura) {
-  imc = peso/altura**2
+  let imc = peso/altura**2
   return imc
 }
-let peso1 = parseFloat(prompt(`Digite seu peso`))
-let altura1 = parseFloat(prompt(`Digite sua altura`))
-
-console.log(parseFloat(calculaIMC(peso1, altura1)))
 
 
 // EXERCÍCIO 04
 function imprimeInformacoesUsuario(nome, idade, email) {
-  return `Meu nome é ${nome}, tenho ${idade} anos, e o meu email é ${email}.`
+  const mensagem = prompt(`Digite seu nome`)
+  const mensagem2 = parseFloat(prompt(`Digite sua idade`))
+  const mensagem3 = prompt(`Digite seu e-mail`)
+  console.log(`Meu nome é ${mensagem}, tenho ${mensagem2} anos, e o meu email é ${mensagem3}.`)
 
 }
-let nome1 = prompt(`Qual seu nome?`)
-let idade1 = parseFloat(prompt(`Qual sua idade?`))
-let email1 = prompt(`Qual seu e-mail?`)
-
-console.log(imprimeInformacoesUsuario(nome1, idade1, email1))
 
 
 // EXERCÍCIO 05
 function imprimeTresCoresFavoritas(cor1, cor2, cor3) {
-  let arrayCores = [cor1, cor2, cor3]
-  return arrayCores
+  const mensagem = prompt(`Digite sua cor favorita`)
+  const mensagem2 = prompt(`Digite sua segunda cor favorita`)
+  const mensagem3 = prompt(`Digite sua terceira cor favorita`)
+  console.log([mensagem, mensagem2, mensagem3])
 }
-let cor1 = prompt(`Diga sua cor favorita`)
-let cor2 = prompt(`Diga outra cor favorita`)
-let cor3 = prompt(`Mais uma cor que você ame, por favor!`)
 
-console.log(imprimeTresCoresFavoritas(cor1, cor2, cor3))
 
 // EXERCÍCIO 06
 function retornaStringEmMaiuscula(string) {
@@ -102,27 +91,50 @@ function retornaPrimeiroElemento(array) {
 
 // EXERCÍCIO 10
 function retornaUltimoElemento(array) {
-  elementoFinal = array.slice[-1]
-  return elementoFinal
-
+  let posicao = array[array.length-1]
+  return posicao
 }
-console.log(retornaPrimeiroElemento([2, 3, 4]))
+ 
 
 // EXERCÍCIO 11
 function trocaPrimeiroEUltimo(array) {
-  // implemente sua lógica aqui
+  let primeira = array[0]
+  let ultima = array[array.length-1]
 
+  array[0] = ultima
+  array[array.length-1] = primeira
+  return array
 }
+
+
 
 // EXERCÍCIO 12
 function checaIgualdadeDesconsiderandoCase(string1, string2) {
-  // implemente sua lógica aqui
-
+  let letraminuscula1 = string1.toLowerCase()
+  let letraminuscula2 = string2.toLowerCase()
+  tamanhosIguais = letraminuscula1 === letraminuscula2
+  return tamanhosIguais
 }
 
-// EXERCÍCIO 13
+
+/* // EXERCÍCIO 13
 function checaRenovacaoRG() {
-  // implemente sua lógica aqui
+ A função deve imprimir 
+  no console um booleano (true ou false) que indica se a carteira precisa ser renovada ou não.
+   A carteira precisa ser renovada segundo os seguintes critérios:
+
+- Para pessoas com menos de 20 anos, ou exatamente 20 anos, deve ser renovada de 5 em 5 anos
+(se for exatamente 5 anos, deve ser renovada).
+- Para pessoas entre 20 e 50 anos, ou exatamente 50, deve ser renovada de 10 em 10 anos
+(se for exatamente 10 anos, deve ser renovada).
+- Para pessoas acima dos 50 anos, deve ser renovada de 15 em 15 anos
+  const mensagem = parseFloat(prompt(`Digite o ano em que estamos`))
+  const mensagem2 = parseFloat(prompt(`Digite de seu nascimento`))
+  const mensagem3 = parseFloat(prompt(`Digite o ano em que sua carteira de identidade foi emitida`))
+  const idade = (mensagem - mensagem2)
+  const renovaocao1 =  
+  if (idade <= 20) &&  
+  if  (20 < idade <= 50) 
 
 }
 
@@ -134,6 +146,11 @@ function checaAnoBissexto(ano) {
 
 // EXERCÍCIO 15
 function checaValidadeInscricaoLabenu() {
-  // implemente sua lógica aqui
-
+  const mensagem = prompt(`Você tem mais de 18 anos?`)
+  const mensagem2 = prompt(`Você possui ensino médio completo?`)
+  const mensagem3 = prompt(`Você possui disponibilidade exclusiva durante os horários do curso?`)
+  const simOuNao = (mensagem) && (mensagem2) && (mensagem3)
+  const yesOrNo = (simOuNao) 
+  console.log (yesOrNo)
 }
+*/
